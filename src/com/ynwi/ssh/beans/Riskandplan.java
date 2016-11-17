4299deeb784b483153f8754f21640613e9a2b944
hasee <hasee@172.19.130.163>
2016-11-17 18:57:29 +0800

    RAP bean

---
+++ src/com/ynwi/ssh/beans/Riskandplan.java
@@ -0,0 +1,26 @@
+package com.ynwi.ssh.beans;
+
+public class Riskandplan {
+	private int idriskandplan;
+	private String planid;
+	private String riskid;
+	public int getIdriskandplan() {
+		return idriskandplan;
+	}
+	public void setIdriskandplan(int idriskandplan) {
+		this.idriskandplan = idriskandplan;
+	}
+	public String getPlanid() {
+		return planid;
+	}
+	public void setPlanid(String planid) {
+		this.planid = planid;
+	}
+	public String getRiskid() {
+		return riskid;
+	}
+	public void setRiskid(String riskid) {
+		this.riskid = riskid;
+	}
+	
+}
